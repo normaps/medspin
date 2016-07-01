e2039fb6236364b2fe6978584e6ea12145e8842f
gemmaan <gemmaan.adhatien@gmail.com>
2016-07-01 16:52:42 +0700

    Add files via upload

---
+++ html/peserta_infolomba.php
@@ -0,0 +1,57 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta name="description" content="MEDSPIN FK UNAIR 2015, Olimpiade Kedokteran Untuk Siswa SMA & Sederajat Se-Asia Tenggara Diselenggarakan Oleh FK UNAIR Pada 8, 21, & 22 November 2015"> 
+  <meta name="keywords" content="Login Medspin FK UNAIR, Medspin 2015, Olimpiade Kedokteran, Medspin FK UNAIR 2015">
+  <meta name="author" content="Gemma Adhatien & Norma Puspitasari">
+  <meta charset="UTF-8"> 
+
+  <title>PESERTA MEDSPIN FK UNAIR 2016</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+  <style type="text/css">
+    *{
+      font-family: 'Open Sans', sans-serif;
+    }
+  </style>
+</head>
+<body style="background:url(http://medspinfkunair.com/pq/images/bg-w.png);">
+
+<nav class="navbar navbar-inverse" style="color:white; background-color:#028fcc; border-color:#028fcc;">
+  <div class="container-fluid">
+    <div class="navbar-header" style="margin:10px;">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span> 
+      </button>
+      <h5>MEDSPIN FK UNAIR 2016</h5>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li><a href="peserta_dashboard.php" style="color:white;">TIM</a></li>
+        <li class="active"><a href="peserta_infolomba.php" style="color:white;">INFO LOMBA</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+              <li><a href="logout.php" style="color:white;"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="container">
+<center>
+  <h1>Coming Soon....</h1>
+</center>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="https://www.atlasestateagents.co.uk/javascript/tether.min.js"></script><!-- Tether for Bootstrap -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+</body>
+</html>
+
