de38623a2699544dbd8d5c2a2e6befaf13469a0a
Norma Puspitasari <normapus1@gmail.com>
2016-07-02 02:58:41 +0700

    Add files
    back = redirect setelah signup isinya ucapan selamat
    password = fungsi encrypt decrypt
    footer tinggal di require disemua page

---
+++ html/back.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<?php
+    require 'connect.php';
+    $conn = connectDB();
+?>
+<head>
+  <meta name="description" content="MEDSPIN FK UNAIR 2015=6, Olimpiade Kedokteran Untuk Siswa SMA & Sederajat Se-Asia Tenggara Diselenggarakan Oleh FK UNAIR Pada 8, 21, & 22 November 2015"> 
+  <meta name="keywords" content="Login Medspin FK UNAIR, Medspin 2015, Olimpiade Kedokteran, Medspin FK UNAIR 2016">
+  <meta name="author" content="Gemma Adhatien & Norma Puspitasari">
+  <meta charset="UTF-8"> 
+
+  <title>PENDAFTARAN ONLINE MEDSPIN FK UNAIR 2016</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <style type="text/css">
+    *{
+      font-family: 'Open Sans', sans-serif;
+    }
+  </style>
+</head>
+
+<body style="background:url(http://medspinfkunair.com/pq/images/bg-w.png);">
+
+<div clas="row" style=" font-size:20; font-weight:700; color:white;  background-color:#028fcc; padding:20px 50px; top:0; left:0; right:0; text-align:center;">
+  <h2>PENDAFTARAN ONLINE MEDSPIN 2016</h2>
+</div>
+  <div class="row" style="margin:0 auto; padding:20px;">
+    <div class="col-sm-8 col-sm-offset-2" style="margin-top:40px;">
+        <div class="panel panel-default" id= "card-info" style="height:350px;">
+          <div class="panel-heading">
+            <h4 style = 'text-align: center;'>TERIMA KASIH, KAMU TELAH TERDAFTAR DI MEDSPIN 2016!</h4>
+          </div>
+          <div class="panel-body">
+            <p> Setelah proses verifikasi, kami akan mengirimkan email yang berisi username dan password akun MEDSPIN 2016 kepada ketua tim kamu.</p>
+            <p>Selamat berlomba!</p>
+            <div class="col-md-4 col-sm-offset-4 text-center"> 
+              <a href="http://medspinfkunair.com" class="btn btn-info btn-lg center-block">
+                <span class="glyphicon glyphicon-arrow-right"></span> Back to Home
+              </a>
+            </div>
+          </div>
+        </div>
+    </div>
+  </div>
+<?php
+  require 'footer.php';
+?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="https://www.atlasestateagents.co.uk/javascript/tether.min.js"></script><!-- Tether for Bootstrap -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+</body>
---
+++ html/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer row-fluid" style="background:#028fcc;">
+	<div class="container">
+    	<div class="navbar-static-bottom" style="color:#fff">
+      		<p class="text-center" style="padding-top:10px;">&copy;  2016 Newbeez</p>
+    	</div>
+	</div>
+</footer>
